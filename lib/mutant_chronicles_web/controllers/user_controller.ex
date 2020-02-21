d954cb3c62bc5d1a572753fd557fc142c8e413d1
Emil Vikström <emil.vikstrom@tv4.se>
2020-02-21 11:39:00 +0100

    Revert "Started to work on characters"
    This reverts commit c3ec4eea9d613dbb699b8776318ecc38c77d1f2d.

--- lib/mutant_chronicles_web/controllers/user_controller.ex
+++ lib/mutant_chronicles_web/controllers/user_controller.ex
@@ -28,35 +28,6 @@ defmodule MutantChroniclesWeb.UserController do
     end
   end
 
-  def new_character(conn, %{"character" => character}) do
-    token = extract_bearer(conn)
-
-    case authenticate(token) do
-      {:ok, user_id} ->
-        case User.insert_character(user_id, character) do
-          :ok ->
-            conn
-            |> put_status(200)
-            |> json(%{"nyi" => "nyi"})
-
-          {:error, reason} ->
-            conn
-            |> put_status(400)
-            |> json(%{"error" => reason})
-        end
-
-      {:error, :no_token} ->
-        conn
-        |> put_status(:unauthorized)
-        |> json(%{"error" => "no bearer"})
-
-      {:error, :invalid_token} ->
-        conn
-        |> put_status(:unauthorized)
-        |> json(%{"error" => "invalid token"})
-    end
-  end
-
   defp extract_bearer(conn) do
     case get_req_header(conn, "authorization") do
       ["Bearer " <> token] -> token
